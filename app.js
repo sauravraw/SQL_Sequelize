d62d81891bf11fcf7f9096b2b157dfe807ed25e2
sauravraw <saurav.upadhyay@raweng.com>
2021-03-08 10:45:20 +0530

    comment added with config.env file

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const app = express();
 const db = require("./models");
+const dotenv = require("dotenv");
+
+// Configure dotenv
+dotenv.config({
+  path: "./config.env",
+});
+
+// Setup the server port
 const PORT = process.env.PORT || 3000;
 
+// Request data content
 app.use(express.urlencoded({ extended: true }));
+
+// Request data content type application/json
 app.use(express.json());
 
+// Import data routes
 const apiRouters = require("./routes/apiRoutes");
-app.use("/api", apiRouters);
 
+// create data routes
+app.use("/", apiRouters);
+
+// Authencticate to backend and if sucesses then listen to the port
 db.sequelize.authenticate().then(() => {
   app.listen(PORT, () => {
     console.log(`Listening on http://localhost:${PORT}`);
